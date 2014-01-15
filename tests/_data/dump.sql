128656434b70f957e6c2aa2d2cda99178dc6a0b1
Adrien Desfourneaux <adrien.desfourneaux@gmail.com>
2014-01-15 03:34:34 +0100

    Configure Db for Codeception acceptance tests

--- tests/_data/dump.sql
+++ tests/_data/dump.sql
@@ -1,1 +1,22 @@
 /* Replace this file with actual dump of your database */
+
+CREATE TABLE user
+(
+    user_id       INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    username      VARCHAR(255) DEFAULT NULL UNIQUE,
+    email         VARCHAR(255) DEFAULT NULL UNIQUE,
+    display_name  VARCHAR(50) DEFAULT NULL,
+    password      VARCHAR(128) NOT NULL,
+    state         SMALLINT
+);
+
+
+CREATE TABLE project
+(
+    project_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    display_name VARCHAR(50) NOT NULL UNIQUE,
+    begin_date INTEGER NOT NULL,
+    end_date INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user (user_id)
+);
