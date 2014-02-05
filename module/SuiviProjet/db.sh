a91bb0bf97406029983bc442f9b3ddc65854352b
Adrien Desfourneaux <adrien.desfourneaux@gmail.com>
2014-02-05 13:38:05 +0100

    Refactor all scripts in qa.sh, improve and move db.sh in SuiviProjet module

---
+++ module/SuiviProjet/db.sh
@@ -0,0 +1,133 @@
+#!/bin/sh
+
+# /*!
+#     Utilitaire de base de données
+#     @author Adrien Desfourneaux <adrien.desfourneaux@gmail.com>
+#  */
+
+DBNAME=suiviprojet
+
+# /*!
+#     Change de répertoire vers le répertoire du script
+#  */
+cdscriptpath () {
+  if [ -z $SCRIPTPATH ]; then
+    SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P )
+    cd $SCRIPTPATH
+  fi
+}
+
+# /*!
+#     Affiche un message de confirmation
+#  */
+confirm () {
+  while true; do
+      read -p "Continuer ? " on
+      case $on in
+          [Oo]* ) break;;
+          [Nn]* ) exit;;
+          * ) echo "Repondre par oui ou non.";;
+      esac
+  done
+}
+
+# /*!
+#     Cré les bases de données de test et de développement du module
+#  */
+create () { 
+  cdscriptpath
+
+  if [ -f data/$DBNAME.sqlite ]; then
+    printf "Attention! Lancer ce script va supprimer la base de données de développement $DBNAME ainsi que tout son contenu.\n"
+    confirm
+    rm data/$DBNAME.sqlite
+  fi
+
+  cat data/zfcuser.sqlite.sql \
+    data/bjyauthorize.sqlite.sql \
+    data/dzuser.sqlite.sql \
+    data/dzproject.sqlite.sql \
+    data/dztask.sqlite.sql \
+    data/suiviprojet.sqlite.sql \
+  | sqlite3 data/suiviprojet.sqlite
+
+  chmod g+w data/$DBNAME.sqlite
+
+  if [ ! -f tests/_data/$DBNAME.sqlite ]; then
+    sqlite3 "" tests/_data/$DBNAME.sqlite
+    chmod g+w tests/_data/$DBNAME.sqlite
+  fi
+}
+
+# /*!
+#     Remplit la base de données de développement du module
+#     avec des données de test
+#  */
+dump () {
+  create
+
+  cat data/dzuser.dump.sqlite.sql \
+    data/dzproject.dump.sqlite.sql \
+    data/dztask.dump.sqlite.sql \
+    data/suiviprojet.dump.sqlite.sql \
+  | sqlite3 data/suiviprojet.sqlite;
+}
+
+# /*!
+#     Restaure les permissions des base de données
+#     de test et de développement du module
+#     ainsi que celle de production
+#  */
+perm () {
+  cdscriptpath
+
+  chmod 770 data
+  if [ -f data/$DBNAME.sqlite ]; then chmod 660 data/$DBNAME.sqlite; fi
+  if [ -d tests/_data ]; then chmod 770 tests/_data; fi
+  if [ -f tests/_data/$DBNAME.sqlite ]; then chmod 660 tests/_data/$DBNAME.sqlite; fi
+  if [ -d ../../data ]; then chmod 770 ../../data; fi
+  if [ -f ../../data/suivi-projet.sqlite ]; then chmod 660 ../../data/suivi-projet.sqlite; fi
+}
+
+# /*!
+#     Copie la base de données de développement du module
+#     en base de données de production de l'application
+#  */
+function prod
+{
+  cdscriptpath;
+
+  if [ -f ../../data/suivi-projet.sqlite ]; then
+    printf "Attention! Vous êtes sur le point de supprimer la base de données de production de l'application suivi-projet!\n";
+    confirm;
+
+    timestamp=$(date +%s)
+
+    cp ../../data/suivi-projet.sqlite ../../data/suivi-projet_$timestamp.sqlite
+    printf "Un copie de sauvegarde de la base de données de production a été faite.\n"
+    printf "/data/suivi-projet_$timestamp.sqlite";
+  fi
+
+  cp data/suiviprojet.sqlite ../../data/suivi-projet.sqlite
+  chmod g+w ../../data/suivi-projet.sqlite
+}
+
+# /*!
+#     Affiche un message d'aide
+#  */
+help () {
+  printf "Usage: db.sh [action]\n"
+  printf "help\taffiche cette aide\n"
+  printf "create\tcre la base de donnees\n"
+  printf "dump\tcre la base de donnees et y met les données de développement\n"
+  printf "perm\trestaure les permissions des bases de données de test, développement et production\n"
+  printf "prod\tcopie la bdd de développement du module en bdd de production de l'application\n"
+}
+
+if [ $# -eq 0 ]; then help
+elif [ "$1" = "help" ]; then help
+elif [ "$1" = "create" ]; then create
+elif [ "$1" = "dump" ]; then dump
+elif [ "$1" = "perm" ]; then perm
+elif [ "$1" = "prod" ]; then prod
+fi
---
+++ module/SuiviProjet/qa.sh
@@ -0,0 +1,209 @@
+#!/bin/sh
+
+# /*!
+#     Script d'assurance qualité (Quality Assurance)
+#     Vérifie la qualité du code
+#     @author Adrien Desfourneaux <adrien.desfourneaux@gmail.com>
+#  */
+
+# /*!
+#     Change de répertoire vers le répertoire du script
+#  */
+cdscriptpath () {
+  if [ -z $SCRIPTPATH ]; then
+    SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P )
+    cd $SCRIPTPATH
+  fi
+}
+  
+# /*!
+#     Lance les tests de spécification
+#  */
+runspec () {
+  cdscriptpath
+  ../../vendor/bin/phpspec run
+}
+
+# /*!
+#     Lance les tests d'acceptation
+#  */
+runcept () {
+  cdscriptpath
+  ../../vendor/bin/codecept run
+}
+
+# /*!
+#     Lance le code sniffer
+#  */
+runcodesniffer () {
+  cdscriptpath
+  ../../vendor/bin/phpcs --standard="phpcs.xml" --ignore="/doc/" --extensions="php,phtml" .
+}
+
+# /*!
+#     Lance le mess detector
+#  */
+runmessdetector () {
+  cdscriptpath
+  ../../vendor/bin/phpmd . text phpmd.xml --exclude "doc,tests*Guy.php" --suffixes "php,phtml"
+}
+
+# /*!
+#     Lance le copy-paste detector
+#  */
+runcpdetector () {
+  cdscriptpath
+  ../../vendor/bin/phpcpd --progress .
+}
+
+# /*!
+#     Génère les statistiques du module
+#  */
+genstats () {
+  cdscriptpath
+  mkdir -p metrics
+  ../../vendor/bin/phploc --progress . > metrics/stats.txt
+}
+
+# /*!
+#     Génère les métriques du module
+#  */
+gendepend () {
+  cdscriptpath
+  mkdir -p metrics
+  ../../vendor/bin/pdepend --summary-xml="metrics/summary.xml" \
+                           --jdepend-chart="metrics/jdepend.svg" \
+                           --overview-pyramid="metrics/pyramid.svg" \
+                           .
+}
+
+# /*!
+#     Génére la documentation du module
+#  */
+gendoc () {
+  cdscriptpath
+  mkdir -p doc
+  ../../vendor/bin/phpdoc.php run -d . -t doc
+}
+
+# /*!
+#     Vérifie la documentation (docblocks) du module
+#  */
+checkdoc () {
+  cdscriptpath
+
+  Category="(\<Config\>|\<Autoload\>|\<Source\>|\<Spec\>|\<Test\>)$"
+  Package="\<SuiviProjet(/.*|\>)"
+  Author="Adrien Desfourneaux <adrien.desfourneaux@gmail\.com>$"
+  License="\shttp://opensource.org/licenses/MIT The MIT License$"
+  Link="\<https://github.com/adrien-desfourneaux/suivi-projet/tree/master/module/SuiviProjet/.+"
+  exclude=".*tests.*Guy.php$"
+
+  find . \( -name "*.php" -o -name "*.phtml" \) -and -not -regex "$exclude" | xargs grep -E -sL "@category\s+${Category}" | awk '{print "Wrong or no category in "$1}'
+  find . \( -name "*.php" -o -name "*.phtml" \) -and -not -regex "$exclude" | xargs grep -E -sL "@package\s+${Package}" | awk '{print "Wrong or no package name in "$1}'
+  find . \( -name "*.php" -o -name "*.phtml" \) -and -not -regex "$exclude" | xargs grep -E -sL "@author\s+${Author}" | awk '{print "Wrong or no author in "$1}'
+  find . \( -name "*.php" -o -name "*.phtml" \) -and -not -regex "$exclude" | xargs grep -E -sL "@license\s+${License}" | awk '{print "Wrong or no license in "$1}'
+  find . \( -name "*.php" -o -name "*.phtml" \) -and -not -regex "$exclude" | xargs grep -E -sL "@link\s+${Link}" | awk '{print "Wrong or no link in "$1}'
+}
+
+# /*!
+#     Affiche l'aide
+#  */
+help () {
+  printf "Usage: qa.sh [command]\n"
+  printf "help\t\taffiche cette aide\n"
+  printf "help [command]\taffiche l'aide de la commande\n"
+  printf "test\t\tgestion des tests du module\n"
+  printf "analyse\t\tanalyse la structure et la syntaxe du code\n"
+  printf "metric\t\tanalyse la métrique du code\n"
+  printf "doc\t\tgestion de la documentation du module\n"
+  printf "\naffiche cette aide si aucune action n'est spécifiée\n"
+}
+
+# /*!
+#     Affiche l'aide de test
+#  */
+helptest () {
+  printf "Usage: qa.sh test [arg]\n"
+  printf "spec\tLance les tests de spécification\n"
+  printf "cept\tLance les tests d'acceptation\n"
+}
+
+# /*!
+#      Affiche l'aide de analyse
+#  */
+helpanalyse () {
+  printf "Usage: qa.sh analyse [arg]\n"
+  printf "code\tvéfifie la syntaxe du code\n"
+  printf "mess\trecherche des problmes potentiels dans le code\n"
+  printf "cp\trecherche des traces de copier-coller dans le code\n"
+  printf "\nLance tous les arguments si aucun n'est spécifié\n"
+}
+
+# /*!
+#     Affiche l'aide de metric
+#  */
+helpmetric () {
+  printf "Usage: qa.sh metric [arg]\n"
+  printf "depend\tGénère des diagramme de dépendances du code\n"
+  printf "stats\tGénère des statistiques sur le code\n"
+  printf "\nLance tous les arguments si aucun n'est spécifié\n"
+}
+
+# /*!
+#     Affiche l'aide de doc
+#  */
+helpdoc () {
+  printf "Usage: qa.sh doc [action]\n"
+  printf "check\tvérifie les blocs de documentation de code\n"
+  printf "gen\tgénère la documentation du module\n"
+  printf "\nLance toutes les actions si aucune n'est spécifiée\n"
+}
+
+# no argument
+if [ $# -eq 0 ]; then help
+
+# help
+elif [ $1 = 'help' ]; then
+  if [ $# -eq 1 ]; then help
+  elif [ $2 = 'test' ]; then helptest
+  elif [ $2 = 'analyse' ]; then helpanalyse
+  elif [ $2 = 'metric' ]; then helpanalyse
+  elif [ $2 = 'doc' ]; then helpdoc
+  fi
+
+# test
+elif [ $1 = 'test' ]; then
+  if [ $# -eq 1 ]; then runspec; runcept
+  elif [ $2 = 'spec' ]; then runspec
+  elif [ $2 = 'cept' ]; then runcept
+  else helptest
+  fi
+
+# analyse
+elif [ $1 = 'analyse' ]; then
+  if [ $# -eq 1 ]; then runcodesniffer; runmessdetector; runcpdetector
+  elif [ $2 = 'code' ]; then runcodesniffer
+  elif [ $2 = 'mess' ]; then runmessdetector
+  elif [ $2 = 'cp' ]; then runcpdetector
+  else helpanalyse
+  fi
+
+# metric
+elif [ $1 = 'metric' ]; then
+  if [ $# -eq 1 ]; then gendepend; genstats
+  elif [ $2 = 'depend' ]; then gendepend
+  elif [ $2 = 'stats' ]; then genstats
+  else helpmetric
+  fi
+
+# doc
+elif [ $1 = 'doc' ]; then
+  if [ $# -eq 1 ]; then checkdoc; gendoc
+  elif [ $2 = 'check' ]; then checkdoc
+  elif [ $2 = 'gen' ]; then gendoc
+  else helpdoc
+  fi
+
+fi
+
