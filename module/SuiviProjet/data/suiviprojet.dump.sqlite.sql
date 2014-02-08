f064975eceac7f1c111f027eb61167d2f3c212b9
Adrien Desfourneaux <adrien.desfourneaux@gmail.com>
2014-02-08 20:47:48 +0100

    New projects and tasks added on other modules

--- module/SuiviProjet/data/suiviprojet.dump.sqlite.sql
+++ module/SuiviProjet/data/suiviprojet.dump.sqlite.sql
@@ -1,8 +1,14 @@
 UPDATE project SET user_id = 1 WHERE project_id = 1;
 UPDATE project SET user_id = 1 WHERE project_id = 2;
 UPDATE project SET user_id = 1 WHERE project_id = 3;
+UPDATE project SET user_id = 1 WHERE project_id = 4;
+UPDATE project SET user_id = 1 WHERE project_id = 5;
+UPDATE project SET user_id = 1 WHERE project_id = 6;
 
 UPDATE task SET project_id = 1 WHERE task_id = 1;
 UPDATE task SET project_id = 1 WHERE task_id = 2;
 UPDATE task SET project_id = 1 WHERE task_id = 3;
 UPDATE task SET project_id = 1 WHERE task_id = 4;
+UPDATE task SET project_id = 1 WHERE task_id = 5;
+UPDATE task SET project_id = 1 WHERE task_id = 6;
+UPDATE task SET project_id = 1 WHERE task_id = 7;
